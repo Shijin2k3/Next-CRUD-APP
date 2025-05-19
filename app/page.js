c927fd473c89b39f067d08a9eb15b3a0adf663cc
Shijin2k3 <shijinkumar2003@gmail.com>
2025-05-19 10:48:52 +0530

    Initialized Post page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Posts from "./posts/page";
 
 export default function Home() {
   return (
     <div className="">
-       <h1> </h1>
+       <Posts />
     </div>
   );
 }
---
+++ app/posts/page.js
@@ -0,0 +1,7 @@
+export default function Posts(){
+    return(
+        <div>
+            <h1 className="text-center">Hello</h1>
+        </div>
+    )
+}
